aaf7440792086dfedf75e771b87d943cb5bc44fc
nrgrandey <nrgrandey@pm.me>
2025-06-13 21:42:38 -0400

    added character_count function to stats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import character_count, word_count
 
 
 # Function to recieve a book's file path and output the contents in a string format
@@ -9,7 +9,9 @@ def get_book_text(book_path):
 
 def main():
     num_words = word_count(get_book_text("books/frankenstein.txt"))
+    char_dict = character_count(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    print(char_dict)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,17 @@
+# Function to count the amound of words in a string
 def word_count(text):
     num_words = 0
     word_list = text.split()
     for word in word_list:
         num_words += 1
     return num_words
+
+def character_count(text):
+    text = text.lower()
+    char_dict = {}
+    for char in text:
+        if char in char_dict:
+            char_dict[char] = char_dict[char] + 1
+        else:
+            char_dict[char] = 1
+    return char_dict
